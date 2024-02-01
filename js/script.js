72fc77a7cc74a46b6dd83d92585a9e54676e0901
Techxuryofficial <techxuryofficial@gmail.com>
2024-02-01 19:29:25 +0530

    bootstrap 5

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+
+var typed = new Typed('.second_text', {
+  strings: ['Web Devlopment', 'Data Science', 'Stock Marketing', 'Game Developer'],
+  typeSpeed: 150,
+  backspeed: 150,
+  loop: true
+});
+
+
+var typed = new Typed('#ft_fol_move', {
+  strings: ['Instagram', 'facebook', 'Twitter'],
+  typeSpeed: 150,
+  backspeed: 150,
+  loop: true
+});
